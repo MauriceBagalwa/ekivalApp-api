41abbaba09682d58519caf50b567a722cb1e9a9c
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2022-03-07 19:03:02 +0100

    rebuild tsoa

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,4 +1,36 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,6 +39,7 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const error_middleware_1 = __importDefault(require("./middleware/error.middleware"));
 const logger_1 = __importDefault(require("./utils/logger"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const routes_1 = require("../dist/routes");
 const helmet_1 = __importDefault(require("helmet"));
 const mongoose_1 = __importDefault(require("mongoose"));
@@ -36,15 +69,9 @@ class App {
         // this.express.use("/pdf", express.static(__dirname + "/public/pdf"));
         this.express.use(body_parser_1.default.json());
         (0, routes_1.RegisterRoutes)(this.express);
-        // this.express.use(
-        //   "/docs",
-        //   swaggerUi.serve,
-        //   async (_req: ExRequest, res: ExResponse) => {
-        //     return res.send(
-        //       swaggerUi.generateHTML(await import("../dist/swagger.json"))
-        //     );
-        //   }
-        // );
+        this.express.use("/docs", swagger_ui_express_1.default.serve, (_req, res) => __awaiter(this, void 0, void 0, function* () {
+            return res.send(swagger_ui_express_1.default.generateHTML(yield Promise.resolve().then(() => __importStar(require("../dist/swagger.json")))));
+        }));
         this.express.use(function errorHandler(err, req, res, next) {
             if (err instanceof tsoa_1.ValidateError) {
                 console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
