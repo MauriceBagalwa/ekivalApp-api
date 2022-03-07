f28bd2b4e8a35a427036207bf13c31c86469d5e2
Maurice Bagalwa <mauricebagalwa009@gmail.com>
2022-03-07 17:00:03 +0100

    update procfile path

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import App from "./app";
 import config from "config";
 
-const app = new App(config.get<number>("port"),config.get<string>("dbUrl"));
+const PORT: any = process.env.PORT || config.get<number>("port")
+const app = new App(PORT, config.get<string>("dbUrl"));
 app.listen();
